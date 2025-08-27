fce3fd984779d887f015b68812342e9f0ec4c06f
MMD\hanya <hanyangma0195224@gmail.com>
2025-08-27 22:28:48 +0800

    Updated Tarnia to be able to add tasks and output the existing list.
    Also changed some formatting with \n as println already prints a new line, set list as 100 entries as prompt suggests input will not exceed 100.

--- src/main/java/Tarnia.java
+++ src/main/java/Tarnia.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 public class Tarnia {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm 𝓣𝓪𝓻𝓷𝓲𝓪 💔\n");
-        System.out.println("What can I do for you?\n");
+        System.out.println("Hello! I'm 𝓣𝓪𝓻𝓷𝓲𝓪 💔");
+        System.out.println("What can I do for you?");
 
         String line;
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int counter = 0;
         while (true) {
             line = in.nextLine();
-            if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon! xo xo\n");
+            if (line.equals("list")){
+                for (int i = 0; i < counter; i ++){
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+            }
+            else if (line.equals("bye")) {
+                System.out.println("BYE BYE. Hope to see you again soon! xo xo");
                 break;
             }
-            System.out.println(line);
+            else {
+                list[counter] = line;
+                System.out.println("added for ya: " + list[counter]);
+                counter++;
+            }
         }
     }
 }
