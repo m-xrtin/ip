5a056533c968d2ff2b88dd850dbd9db58bc32a04
MMD\hanya <hanyangma0195224@gmail.com>
2025-08-30 12:39:54 +0800

    Created Task Class to better manage list
    Also fixed up the logic, changing from if/else statements to Switch/Case statements for simplicity

--- src/main/java/Tarnia.java
+++ src/main/java/Tarnia.java
@@ -1,29 +1,57 @@
 import java.util.Scanner;
+
 public class Tarnia {
     public static void main(String[] args) {
         System.out.println("Hello! I'm 𝓣𝓪𝓻𝓷𝓲𝓪 💔");
         System.out.println("What can I do for you?");
 
-        String line;
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] tasks = new Task[100];
         int counter = 0;
+
         while (true) {
-            line = in.nextLine();
-            if (line.equals("list")){
-                for (int i = 0; i < counter; i ++){
-                    System.out.println((i + 1) + ". " + list[i]);
+            String line = in.nextLine().trim();
+            // split into command and index
+            String[] parts = line.split(" ", 2);
+            String command = parts[0];
+
+            // Switch is more efficient for these tasks, changed from If/else if statements
+            switch (command) {
+            case "list":
+                System.out.println("Here are the tasks in your list! :");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
-            }
-            else if (line.equals("bye")) {
-                System.out.println("BYE BYE. Hope to see you again soon! xo xo");
                 break;
-            }
-            else {
-                list[counter] = line;
-                System.out.println("added for ya: " + list[counter]);
+
+            case "mark":
+                int mark_index = Integer.parseInt(parts[1]) - 1;
+                tasks[mark_index].markDone();
+                System.out.println("Yayyyy :), I've marked this task as done:");
+                System.out.println("  [" + tasks[mark_index].getStatusIcon() + "] " + tasks[mark_index].getDescription());
+                break;
+
+            case "unmark":
+                // Offsetting the index
+                int unmark_index = Integer.parseInt(parts[1]) - 1;
+                tasks[unmark_index].markUndone();
+                System.out.println("OK :( , I've marked this task as not done yet:");
+                System.out.println("  [" + tasks[unmark_index].getStatusIcon() + "] " + tasks[unmark_index].getDescription());
+                break;
+
+            case "bye":
+                System.out.println("BYE BYE. Hope to see you again soon! xo xo");
+                in.close();
+                return;
+
+            default:
+                // Assume default is task, add to list.
+                tasks[counter] = new Task(line);
+                System.out.println("added for ya: " + "[" + tasks[counter].getStatusIcon() + "] " + tasks[counter].getDescription());
                 counter++;
+                break;
             }
         }
     }
 }
+
